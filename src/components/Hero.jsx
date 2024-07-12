77960ec8735db7fbf7bb2b52f304e254883076cc
Geraldo Kranenburg <geraldo.kranenburg@outlook.com>
2024-07-12 20:05:04 +0800

    finished simple hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,9 +4,19 @@ import { icons } from '../assets'
 const Hero = () => {
   return (
     <header className='w-full flex justify-center items-center flex-col'>
-      <nav className='flex justify-between items-center flex-col'>
-        <img src={icons}/>
+      <nav className='flex justify-between items-center w-full mb-10 pt-3'>
+        <img src={icons} alt ="trans_logo" className='w-28 object-contain' />
+        <button type="button" onClick={()=> window.open('https://github.com/gery117')} className='black_btn'>
+          Github
+        </button>
       </nav>
+      <h1 className='head_text'>
+        Translate Text Super Easy With <br className='max-md:hidden'/>
+        <span className='orange_gradient'>Google</span>
+      </h1>
+      <h2 className='desc'>
+        Easily translate any text with this handy translator app, It translates whatever you want, whenever you want.
+      </h2>
     </header>
   )
 }

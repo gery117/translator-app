7956f5ac656f26ddcb2c6660a09641801a23ab03
Geraldo Kranenburg <geraldo.kranenburg@outlook.com>
2024-07-14 13:33:44 +0800

    added the rapid api key and options

--- src/services/text.js
+++ src/services/text.js
@@ -1,8 +1,30 @@
 // import { createApi } from "@reduxjs/toolkit/query";
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
+const options = {
+    method: 'POST',
+    url: 'https://google-translate113.p.rapidapi.com/api/v1/translator/text',
+    headers: {
+        'x-rapidapi-key': 'e8124269abmshcb92d2fa75b6976p1caf58jsn2f880e7585f0',
+        'x-rapidapi-host': 'google-translate113.p.rapidapi.com',
+        'Content-Type': 'application/json'
+    },
+    data: {
+        from: 'auto',
+        to: 'en',
+        text: 'Xin chào cảm ơn bạn đã sử dụng dịch vụ của chúng tôi'
+    }
+};
+
 export const textApi = createApi({
     reducerPath: 'textApi',
+    baseQuery: fetchBaseQuery({
+        baseUrl: 'https://google-translate113.p.rapidapi.com/api/v1/translator/',
+        prepareHeaders: (headers) => {
+            headers.set('x-rapidapi-key', 'KEY');
+            headers.set('x-rapidapi-host', 'google-translate113.p.rapidapi.com')
+        }
+    }),
     endpoints: (builder) =>({
         getTranslation: builder.query({
             query: (params) => 'test'
